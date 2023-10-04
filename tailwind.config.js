9549de8ff70355ca96e31d0b283ae98fffbc21f6
Evgeny Efimenko <evgeny.efimenko@proton.me>
2023-10-04 13:55:20 +0500

    chore: add some libs
    @dnd-kit-core, @dnd-kit/sortable, tailwindcss, tailwind-merge

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      "background": "#0D1117",
+      "card-background": "#161C22"
+    },
+  },
+  plugins: [],
+}
+
