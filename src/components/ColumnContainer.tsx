36c34319515ae53697494b329b2395ab5b16cf9e
Evgeny Efimenko <evgeny.efimenko@proton.me>
2023-10-06 19:51:27 +0500

    feat: add board column logic

---
+++ src/components/ColumnContainer.tsx
@@ -0,0 +1,37 @@
+import { EllipsisVerticalIcon } from '@heroicons/react/24/outline'
+import { Column, Id } from '../types'
+
+interface Props {
+  column: Column;
+  deleteColumn: (id: Id) => void;
+
+}
+
+function ColumnContainer(props: Props) {
+  const { column, deleteColumn } = props
+  return (
+    <div className="flex h-[500px] max-h-[500px] w-[350px] flex-col rounded-md bg-secondary">
+      <div className="text-md flex h-[60px] cursor-grab items-center  justify-between rounded-md rounded-b-none border-4 border-secondary bg-background p-3 font-bold">
+        <div className="flex items-center gap-2">{column.title}</div>
+        <div className="flex gap-1">
+          <div className="flex items-center justify-center py-1 text-gray-500">
+            0
+          </div>
+          <button
+            type="button"
+            className="rounded px-1 py-1 hover:bg-secondary hover:stroke-white"
+            onClick={() => {
+              deleteColumn(column.id)
+            }}
+          >
+            <EllipsisVerticalIcon className="h-6 stroke-gray-500 " />
+          </button>
+        </div>
+      </div>
+      <div className="flex flex-grow">Content</div>
+      <div>Footer</div>
+    </div>
+  )
+}
+
+export default ColumnContainer
--- src/components/KanbanBoard.tsx
+++ src/components/KanbanBoard.tsx
@@ -1,18 +1,49 @@
 import { PlusCircleIcon } from '@heroicons/react/24/outline'
+import { useState } from 'react'
+import { Column, Id } from '../types'
+import ColumnContainer from './ColumnContainer'
 
 function KanbanBoard() {
+  const [columns, setColumns] = useState<Column[]>([])
+  console.log(columns)
+
   return (
     <div className="m-auto flex min-h-screen w-full items-center overflow-x-auto overflow-y-hidden px-[40px]">
-      <div className="m-auto">
+      <div className="m-auto flex gap-4">
+        <div className='flex gap-4'>
+          {columns.map((col) => (
+          <ColumnContainer column={col} deleteColumn={deleteColumn}/>
+          ))}
+        </div>
         <button
           type="button"
-          className="h-[60px] w-[360px] min-w-[350px] cursor-pointer rounded-lg bg-background border-2 border-secondary p-4 ring-rose-500 hover:ring-2 flex gap-2"
+          className="flex h-[60px] w-[360px] min-w-[350px] cursor-pointer gap-2 rounded-lg border-2 border-secondary bg-background p-4 ring-rose-500 hover:ring-2"
+          onClick={() => {
+            createNewColumn()
+          }}
         >
           <PlusCircleIcon className="h-6 w-6" /> Add Column
         </button>
       </div>
     </div>
   )
+  function createNewColumn() {
+    const columnToAdd: Column = {
+      id: generatedId(),
+      title: `Column ${columns.length + 1}`,
+    }
+
+    setColumns([...columns, columnToAdd])
+  }
+  function deleteColumn(id: Id) {
+    const filteredColumns = columns.filter((col) => col.id !== id)
+    setColumns(filteredColumns)
+  }
+}
+
+function generatedId() {
+  // Generate a random number between 0 and 10000
+  return Math.floor(Math.random() * 10001)
 }
 
 export default KanbanBoard
